9a2afecbfd386658c71a0dc8558e4d5767f64a68
Land-Service-bd <bdenfo@gmail.com>
2026-09-21 03:11:55 +0600

    Restore main API entrypoint after startup diagnosis

--- api/index.js
+++ api/index.js
@@ -1,5 +1,1 @@
-try {
-  module.exports = require('../server');
-} catch (error) {
-  module.exports = (req, res) => res.status(500).json({ ok:false, error: String(error && error.message || error), stack: String(error && error.stack || '').split('\\n').slice(0,8) });
-}
+module.exports = require('../server');
