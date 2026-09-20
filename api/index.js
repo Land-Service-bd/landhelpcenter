cecd2436d372e0c6a6acdfe3570353f849c0b443
Land-Service-bd <bdenfo@gmail.com>
2026-09-21 03:11:04 +0600

    Temporarily isolate Vercel function startup for diagnosis

--- api/index.js
+++ api/index.js
@@ -1,1 +1,1 @@
-module.exports = require('../server');
+module.exports = (req, res) => res.status(200).json({ ok: true, diagnostic: "api-index-loads", node: process.version, vercel: Boolean(process.env.VERCEL) });
